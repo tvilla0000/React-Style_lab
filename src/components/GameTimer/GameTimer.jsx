56262303fcab6c68f74b6e18308a0da9905a8757
tyler villa <bigboiii@tylers-MacBook-Pro.local>
2019-10-10 12:23:45 -0700

    finish

---
+++ src/components/GameTimer/GameTimer.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const GameTimer = props => <div className="timer">00:00</div>;
+
+export default GameTimer;
---
+++ src/components/GuessPeg/GuessPeg.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./GuessPeg.css";
+
+const GuessPeg = props => (
+  <div className="GuessPeg" style={{ backgroundColor: props.color }}></div>
+);
+
+export default GuessPeg;
